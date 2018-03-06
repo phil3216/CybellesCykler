3eb5e327edef105729679bc0129630d72fe2aec1
phil3216 <phil3216@edu.campusvejle.dk>
2018-03-06 10:15:39 +0100

    Created stub DBHandler methods
    +CommonDB implemented
    +Created stub methods for DBHandler

--- DataAccess/CommonDB.cs
+++ DataAccess/CommonDB.cs
@@ -1,13 +1,84 @@
-﻿using System;
+﻿using System.Data.SqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
 
 namespace DataAccess
 {
-    public class CommonDB
+    public abstract class CommonDB
     {
+        protected readonly string connectionString;
+       
+        public CommonDB(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
 
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="command"></param>
+        /// <remarks>
+        /// Was changed from the original string input to an sql command because else sql injection is possible
+        /// </remarks>
+        /// <returns></returns>
+        protected bool ExecuteNonQuery(SqlCommand command)
+        {
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                using (command)
+                {
+                    command.Connection = connection;
+                    command.ExecuteNonQuery();
+                }
+
+                return true;
+            }
+            catch (SqlException)
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="command"></param>
+        /// <param name="type"></param>
+        /// <remarks>
+        /// Was changed from the original string input to an sql command because else sql injection is possible
+        /// </remarks>
+        /// <returns></returns>
+        protected DataSet ExecuteQuery(SqlCommand command,QueryType type)
+        {
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (command)
+            {
+                command.Connection = connection;
+                switch (type)
+                {
+                    case QueryType.StoredProcedure:
+                        command.CommandType = CommandType.StoredProcedure;
+                        break;
+                    case QueryType.Query:
+                        break;
+                    default:
+                        break;
+                }
+
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                DataSet set = new DataSet();
+                adapter.Fill(set);
+                return set;
+            }
+
+        }
     }
 }
---
+++ DataAccess/DBHandler.cs
@@ -0,0 +1,75 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class DBHandler : CommonDB
+    {
+        public DBHandler(string connectionString) : base(connectionString)
+        {
+            //Should be empty
+        }
+
+        public Rentee GetRentee(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public Rentee GetOrder(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public Rentee GetBike(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool NewRentee(Rentee rentee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool NewOrder(Order order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool NewBike(Bike bike)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateRentee(Rentee rentee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateOrder(Order order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateBike(Bike bike)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteRentee(Rentee rentee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteOrder(Order order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteBike(Bike bike)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
