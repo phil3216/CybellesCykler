9b0093dcbcd302cef59a1a3783ea07ecaa9499f2
phil3216 <phil3216@edu.campusvejle.dk>
2018-03-06 11:34:17 +0100

    Implemented DB Gets
    Implemented DB Gets

--- DataAccess/DBHandler.cs
+++ DataAccess/DBHandler.cs
@@ -1,9 +1,12 @@
-﻿using Entities;
+﻿using System.Data.SqlTypes;
+using System.Data.SqlClient;
+using Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
 
 namespace DataAccess
 {
@@ -16,15 +19,119 @@ namespace DataAccess
 
         public Rentee GetRentee(int id)
         {
-            throw new NotImplementedException();
+            SqlCommand command = new SqlCommand("SELECT * FROM Renters WHERE ID=@id");
+            command.Parameters.AddWithValue("@id", id);
+
+            DataSet set = ExecuteQuery(command, QueryType.Query);
+            DataRow row = set.Tables[0].Rows[0];
+
+            string name = (string)row["Name"];
+            string phonenumber = (string)row["Phonenumber"];
+            string address = (string)row["PhysAddress"];
+            DateTime registerDate = (DateTime)row["RegisterDate"];
+
+            return new Rentee(name, address, phonenumber, registerDate, id);
+
         }
-        public Rentee GetOrder(int id)
+        public Order GetOrder(int id)
         {
-            throw new NotImplementedException();
+            SqlCommand command = new SqlCommand("SELECT * FROM Orders WHERE ID=@id");
+            command.Parameters.AddWithValue("@id", id);
+
+            DataSet set = ExecuteQuery(command, QueryType.Query);
+            DataRow row = set.Tables[0].Rows[0];
+
+            DateTime deliveryDate = (DateTime)row["DeliveryDate"];
+            DateTime OrderDate = (DateTime)row["OrderDate"];
+
+            Bike bike = GetBike((int)row["BikeID"]);
+            Rentee rentee = GetRentee((int)row["RenteeID"]);
+
+            return new Order(bike, rentee, OrderDate, deliveryDate, id);
         }
-        public Rentee GetBike(int id)
+        public Bike GetBike(int id)
         {
-            throw new NotImplementedException();
+            SqlCommand command = new SqlCommand("SELECT * FROM Bikes WHERE ID=@id");
+            command.Parameters.AddWithValue("@id", id);
+
+            DataSet set = ExecuteQuery(command, QueryType.Query);
+            DataRow row = set.Tables[0].Rows[0];
+
+            string bikeDescription = (string)row["BikeDescription"];
+            decimal pricePerDay = (decimal)row["PricePerDay"];
+            Bikekind kind = (Bikekind)row["BikeKind"];
+
+            return new Bike(pricePerDay, bikeDescription, kind, id);
+        }
+
+        public List<Bike> GetAllBikes()
+        {
+            SqlCommand command = new SqlCommand("SELECT * FROM Bikes");
+
+            List<Bike> bikes = new List<Bike>();
+
+            DataSet set = ExecuteQuery(command, QueryType.Query);
+
+            foreach (DataRow row in set.Tables[0].Rows)
+            {
+                string bikeDescription = (string)row["BikeDescription"];
+                decimal pricePerDay = (decimal)row["PricePerDay"];
+                Bikekind kind = (Bikekind)row["BikeKind"];
+                int id = (int)row["ID"];
+                bikes.Add(new Bike(pricePerDay, bikeDescription, kind, id));
+            }
+
+            return bikes;
+        }
+
+        public List<Order> GetAllOrders()
+        {
+            List<Bike> bikes = GetAllBikes();
+            List<Rentee> renters = GetAllRentees();
+
+            SqlCommand command = new SqlCommand("SELECT * FROM Orders");
+
+            List<Order> orders = new List<Order>();
+
+            DataSet set = ExecuteQuery(command, QueryType.Query);
+
+            foreach (DataRow row in set.Tables[0].Rows)
+            {
+                DateTime deliveryDate = (DateTime)row["DeliveryDate"];
+                DateTime OrderDate = (DateTime)row["OrderDate"];
+                int id = (int)row["ID"];
+                int bikeID = (int)row["BikeID"];
+                int renteeID = (int)row["RenteeID"];
+
+                Bike bike = bikes.Where(x => x.ID == bikeID).First();
+                Rentee rentee = renters.Where(x => x.ID == renteeID).First();
+
+                orders.Add(new Order(bike, rentee, OrderDate, deliveryDate, id));
+            }
+
+            return orders;
+        }
+
+        public List<Rentee> GetAllRentees()
+        {
+            SqlCommand command = new SqlCommand("SELECT * FROM Renters");
+
+            List<Rentee> renters = new List<Rentee>();
+
+            DataSet set = ExecuteQuery(command, QueryType.Query);
+
+            foreach (DataRow row in set.Tables[0].Rows)
+            {
+                string name = (string)row["Name"];
+                string phonenumber = (string)row["Phonenumber"];
+                string address = (string)row["PhysAddress"];
+                DateTime registerDate = (DateTime)row["RegisterDate"];
+                int id = (int)row["ID"];
+
+                renters.Add(new Rentee(name, address, phonenumber, registerDate, id));
+            }
+
+            return renters;
         }
 
         public bool NewRentee(Rentee rentee)
@@ -72,19 +179,5 @@ namespace DataAccess
             throw new NotImplementedException();
         }
 
-        public List<Bike> GetAllBikes()
-        {
-            throw new NotImplementedException();
-        }
-
-        public List<Order> GetAllOrders()
-        {
-            throw new NotImplementedException();
-        }
-
-        public List<Rentee> GetAllRentees()
-        {
-            throw new NotImplementedException();
-        }
     }
 }
